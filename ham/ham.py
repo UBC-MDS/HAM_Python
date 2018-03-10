ca3029a39e331754accf4af418efe9c0c49f2405
Jordan Dubchak <jordandubchak@gmail.com>
2018-03-10 14:01:25 -0800

    Almost complete vis_missing function

--- ham/ham.py
+++ ham/ham.py
@@ -32,7 +32,7 @@ def todf(data_obj, col_names = None):
         ## if a data frame is inputted, data frame is returned
         return pd.DataFrame(data_obj)
 
-def vis_missing(data_obj, colour=default, missing_val_char=NaN):
+def vis_missing(data_obj, colour="inferno", missing_val_char=np.NaN):
     """
     Author: JD, March 2018 
     This function takes a data frame and returns a visualization of all missing values. 
@@ -48,6 +48,41 @@ def vis_missing(data_obj, colour=default, missing_val_char=NaN):
         - if the data object is not a matrix or data frame, raise an error
         - if the colour mapping is not a valid seaborn mapping, print a warning and use default colours
     """
+    cmaps = ['Accent','Blues','BrBG','BuGn','BuPu','CMRmap','Dark2', 'GnBu',
+             'Greens','Greys','OrRd', 'Oranges','PRGn', 'Paired','Pastel1', 
+             'Pastel2', 'PiYG', 'PuBu','PuBuGn', 'PuOr', 'PuRd', 'Purples','RdBu', 
+             'RdGy', 'RdPu', 'RdYlBu','RdYlGn', 'Reds', 'Set1', 'Set2','Set3', 'Spectral', 
+             'Wistia', 'YlGn','YlGnBu', 'YlOrBr', 'YlOrRd', 'afmhot','autumn', 'binary', 
+             'bone', 'brg','bwr', 'cool', 'coolwarm','copper', 'cubehelix', 'flag', 
+             'gist_earth','gist_gray', 'gist_heat', 'gist_ncar', 'gist_rainbow','gist_stern', 
+             'gist_yarg', 'gnuplot', 'gnuplot2','gray', 'hot', 'hsv', 'inferno','jet', 'magma', 
+             'nipy_spectral','ocean', 'pink','plasma', 'prism', 'rainbow', 'seismic','spring', 
+             'summer', 'tab10', 'tab20','tab20b', 'tab20c', 'terrain','viridis','winter']
+    
+    
+    df = todf(data_obj)
+    
+    ## raise a Type Error if missing val char is not supported
+    if not missing_val_char in [np.NaN, np.NAN, np.nan, "?", " ", ""]:
+        raise TypeError("Missing Value Character is not recognized. \nAccepted missing value characters are: np.NaN, np.NAN, np.nan, '?', ' ', ''")
+    
+    ## if the coded missing value character isn't already a numpy NaN value, convert to np.NaN for plotting 
+    if not missing_val_char in [np.NaN, np.NAN, np.nan]:
+        df.replace({missing_val_char: np.NaN}, inplace=True) 
+    
+    ## if the colour map specified isn't a cmap option, 
+    if colour not in cmaps:
+        warnings.warn("Colour map given is not recognized. Using default inferno.", Warning)
+        colour = "inferno"
+    
+    new_df = np.where(df.isnull(), 1, 0)
+    new_df = pd.DataFrame(np.where(df.isnull(), 1, 0), columns=df.columns)
+    fig = sns.heatmap(new_df, yticklabels=False, cmap=plt.cm.get_cmap(colour, 2))
+    cbar = fig.collections[0].colorbar
+    cbar.set_ticks([0.25,0.75])
+    cbar.set_ticklabels(["Missing Value", "Not Missing Value"])      
+    
+    return fig
 
 
 def impute_missing(df, method, missing_val_char):
