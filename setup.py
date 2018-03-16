2a5f5e834dbaf99557595d8185d4e977f0125afb
Jordan Dubchak <jordandubchak@gmail.com>
2018-03-16 11:43:24 -0700

    Remove list of authors

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
       name='HAM_Python',
       version='v1.0',
-      author=['Duong Vu', 'Jordan Dubchak', 'Linsey'],
+      author='Duong Vu, Jordan Dubchak, Linsey Yuo',
       long_description=open('README.md').read(),
       install_requires=['setuptools', 'pandas','numpy', 'seaborn', 'matplotlib'],
       include_package_data=True
