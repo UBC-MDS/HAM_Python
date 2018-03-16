4ba05ac62550f132d1148ad89d9f88f3abb10505
Jordan Dubchak <jordandubchak@gmail.com>
2018-03-16 11:23:09 -0700

    Update setup file to match master

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(
       name='HAM_Python',
-      author='Duong Vu, Jordan Dubchak, Linsey Yuo',
-      version='3.0',
-      url='http://github.com/UBC-MDS/HAM_Python',
+      version='v1.0',
+      author=['Duong Vu', 'Jordan Dubchak', 'Linsey'],
+      packages=['HAM_Python',],
       license='MIT License',
-      long_description=open('README.txt').read(),
+      long_description=open('README.md').read(),
+      install_requires=['setuptools', 'pandas','numpy', 'seaborn', 'matplotlib'],
+      include_package_data=True
       )
