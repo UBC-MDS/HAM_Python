8c5c964590d1e212c010442c58ba422f9da6804f
Jordan Dubchak <jordandubchak@gmail.com>
2018-03-16 11:24:08 -0700

    Remove packages line

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
       name='HAM_Python',
       version='v1.0',
       author=['Duong Vu', 'Jordan Dubchak', 'Linsey'],
-      packages=['HAM_Python',],
       license='MIT License',
       long_description=open('README.md').read(),
       install_requires=['setuptools', 'pandas','numpy', 'seaborn', 'matplotlib'],
