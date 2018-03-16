b1e72198bd7773cabd9af9a91f4fbb07628a17de
Jordan Dubchak <jordandubchak@gmail.com>
2018-03-16 11:16:56 -0700

    Alter setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
       name='HAM_Python',
-      version='1.0',
-      packages=['HAM_Python',],
+      author='Duong Vu, Jordan Dubchak, Linsey Yuo',
+      version='3.0',
+      url='http://github.com/UBC-MDS/HAM_Python',
       license='MIT License',
-      long_description=open('README.txt').read(),
+      long_description=read('README.txt'),
       )
