329c9b4d3fde4edc1b17ba1370c249d702749dbe
QUARTZRAINNIE\Duong <dqvu@umail.iu.edu>
2018-03-15 19:13:23 -0700

    change setup.py file

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -1,9 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
       name='HAM_Python',
-      version='1.0',
+      version='v1.0',
+      author=['Duong Vu', 'Jordan Dubchak', 'Linsey'],
       packages=['HAM_Python',],
       license='MIT License',
       long_description=open('README.txt').read(),
+      install_requires=['setuptools', 'pandas','numpy', 'seaborn', 'matplotlib'],
+      include_package_data=True
       )
